b1573127fbffb822b6ee3a5261e5a0a48af55aa7
bhavyamishra2209 <bhavyamishra2209@gmail.com>
2026-09-04 01:17:20 +0530

    chore: Clean up backup files and update data files

--- back-end-backup/llm/__init__.py
+++
@@ -1,1 +0,0 @@
-from .model import BaseLLM, OpenAIModel, LocalLLM, create_llm
--- back-end-backup/security/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Security module for document integrity and audit trail.
-"""
-
-from .hash_chain import DocumentHashChain
-from .integrity_checker import IntegrityChecker
-from .audit_logger import AuditLogger
-
-__all__ = ['DocumentHashChain', 'IntegrityChecker', 'AuditLogger']
